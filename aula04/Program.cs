b7be58f2ee95ccc5a26dac2b4efbbdcefe3a8faf
João Pedro <127902784+JoaoRedroRibeiro@users.noreply.github.com>
2024-08-20 23:31:43 +0000

    codigo da aula 04

---
+++ aula04/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    public static void Main(string[]args){
+        Pessoa objPessoa = new Pessoa("joaojoao");
+        Console.WriteLine("ates do metodo (class);" + objPessoa.ToString());
+        AlteraNomeClasse(objPessoa);
+        Console.WriteLine("depois do metodo (class);" + objPessoa.ToString());
+
+        PessoaStruct objPessoaStruct = new PessoaStruct("miguel");
+        Console.WriteLine("antes do metodo (struct):" + objPessoaStruct.ToString());
+        AlteraNomeStruct(objPessoaStruct);
+        Console.WriteLine("depois do metodo (struct):" + objPessoaStruct.ToString());
+
+
+    }
+    public static void AlteraNomeClasse(Pessoa objaux){
+        objaux.Nome = "dani";
+
+    }
+    public static void AlteraNomeStruct(PessoaStruct objaux){
+        objaux.NomeStruct = "mingau";
+    }
+}
